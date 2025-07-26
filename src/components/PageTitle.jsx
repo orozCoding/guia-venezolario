96d4618b605bb46db8df8c6fe020ff9c46d2a00b
Angel Orozco <orozcoding@gmail.com>
2025-07-25 22:54:00 -0400

    update favicon

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -13,14 +13,26 @@ function PageTitle({ children, size = "large" }) {
   }
 
   return (
-    <h1 
-      className={`${getSizeClasses()} font-extrabold text-white text-center leading-tight`}
-      style={{
-        textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000'
-      }}
-    >
-      {children}
-    </h1>
+    <div className="relative inline-block">
+      <h1 
+        className={`${getSizeClasses()} font-extrabold text-center leading-tight relative z-10 px-4`}
+        style={{
+          background: 'linear-gradient(45deg, #fbbf24, #f59e0b, #d97706)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          backgroundClip: 'text',
+          filter: 'drop-shadow(3px 3px 0px #000) drop-shadow(-1px -1px 0px #000) drop-shadow(1px -1px 0px #000) drop-shadow(-1px 1px 0px #000)'
+        }}
+      >
+        {children}
+      </h1>
+      
+      {/* Cartoon decorative elements */}
+      <div className="absolute -top-2 -left-1 w-5 h-5 bg-yellow-400 rounded-full opacity-80 animate-pulse"></div>
+      <div className="absolute -bottom-1 -right-0 w-4 h-4 bg-orange-400 rounded-full opacity-70 animate-pulse" style={{ animationDelay: '0.5s' }}></div>
+      <div className="absolute top-1/2 -left-2 w-3 h-3 bg-yellow-300 rounded-full opacity-80 animate-bounce" style={{ animationDelay: '1s' }}></div>
+      <div className="absolute top-1/4 -right-1 w-2 h-2 bg-orange-300 rounded-full opacity-60 animate-pulse" style={{ animationDelay: '1.5s' }}></div>
+    </div>
   )
 }
 
